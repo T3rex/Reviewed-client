048fdb97224eff4c72a4ca8b90c4b1e16423a392
T3rex <31337aditya@gmail.com>
2025-06-19 21:19:40 +0530

    Added header in review form

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import logo from "../assets/logo-dark.svg";
 function ReviewForm() {
   const { campaignName, campaignId } = useParams();
-  return <div>ReviewForm</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-3xl h-screen bg-[#fafafa] p-4 shadow-md rounded-xl">
+        <div className="w-48">
+          <img src={logo} alt="Logo" className="w-full object-contain" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ReviewForm;
