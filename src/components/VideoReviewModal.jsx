8e3297d6d2565d997e36eb3e5718d7e23907f2d8
T3rex <31337aditya@gmail.com>
2025-06-22 21:15:03 +0530

    Added video recoding functionality with timer

--- src/components/VideoReviewModal.jsx
+++ src/components/VideoReviewModal.jsx
@@ -1,15 +1,77 @@
-import React from "react";
-import { X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { X, VideoOff, Video } from "lucide-react";
 import StarRating from "./StarRating";
+import { Dropdown, DropdownItem, ToggleSwitch } from "flowbite-react";
+
+import {
+  useReactMediaRecorder,
+  ReactMediaRecorder,
+} from "react-media-recorder";
 
 function VideoReviewModal({ setShowVideoReviewModal, formConfig }) {
+  const [timer, setTimer] = useState();
+  const [videoDevices, setVideoDevice] = useState([]);
+  const [audioDevices, setaudioDevices] = useState([]);
   const handleClose = () => setShowVideoReviewModal(false);
   const handleSubmit = () => console.log("Submit");
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({
+      video: true,
+      audio: true,
+    });
+
+  const startTimer = () => {
+    return new Promise((resolve, reject) => {
+      const intervalId = setInterval(() => {
+        setTimer((prev) => {
+          if (prev == 1) {
+            clearInterval(intervalId);
+            resolve();
+
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }).then(() => startRecording());
+  };
+
+  const handleStartRecording = () => {
+    setTimer(3);
+    startTimer();
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      devices.forEach((device) => {
+        // const menu = document.getElementById("input-devices");
+        // if (device.kind === "audioinput") {
+        //   const item = document.createElement("option");
+        //   item.textContent = device.label;
+        //   item.value = device.deviceId;
+        //   menu.appendChild(item);
+        // }
+        if (device.kind === "videoinput") {
+          setVideoDevice((prev) => {
+            console.log(device.label);
+            prev = [...prev, device];
+            return prev;
+          });
+        }
+        if (device.kind === "audioinput") {
+          setaudioDevices((prev) => {
+            prev = [...prev, device];
+            return prev;
+          });
+        }
+      });
+    });
+  }, []);
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm px-4 overflow-y-auto">
       {/* Modal Box */}
-      <div className="relative bg-white rounded-2xl mt-100 shadow-2xl w-full max-w-lg mx-auto p-6 sm:p-8">
+      <div className="relative bg-white rounded-2xl shadow-2xl w-full max-w-lg mx-auto p-6 sm:p-8">
         {/* Close Button */}
         <button
           onClick={handleClose}
@@ -21,16 +83,103 @@ function VideoReviewModal({ setShowVideoReviewModal, formConfig }) {
 
         {/* Modal Content */}
         <div className="flex flex-col gap-6 mt-2">
-          <div className="flex flex-col items-center gap-2 text-center">
-            <h1 className="text-2xl font-semibold">
-              Check Your Camera and Microphone
-            </h1>
-            <p className="text-gray-500 px-2 text-balance">
-              You have up to 180 seconds to record your video. Don't worry: You
-              can review your video before submitting it, and you can re-record
-              if needed.
-            </p>
+          {/* Instructions */}
+          {status === "idle" && (
+            <div className="flex flex-col items-center gap-2 text-center">
+              <h1 className="text-2xl font-semibold">
+                Check Your Camera and Microphone
+              </h1>
+              <p className="text-gray-500 px-2 text-balance">
+                You have up to 180 seconds to record your video. Don't worry:
+                You can review your video before submitting it, and you can
+                re-record if needed.
+              </p>
+            </div>
+          )}
+          {/* Questions */}{" "}
+          {status === "recording" && (
+            <div>
+              <h2 className="text-lg font-bold mb-2">Questions</h2>
+              <ul className="list-disc list-inside text-sm sm:text-base text-gray-700 space-y-1">
+                {formConfig.questions.map((q) => (
+                  <li key={q.id}>{q.question}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {/* Video Recorder */}
+          <div>
+            {/* <p>{status}</p> */}
+            {status == "idle" ? (
+              <div className="h-60 w-full bg-gray-800 text-8xl text-white flex items-center justify-center transition-all duration-300 ease-in-out">
+                {timer}
+              </div>
+            ) : (
+              <video className="w-full h-60" src={mediaBlobUrl} controls />
+            )}
           </div>
+          {/* Device Settings */}
+          {status === "idle" && (
+            <div className="bg-gray-100 p-6 rounded-xl border-gray-300 border-1 flex items-center justify-between">
+              <h2 className="text-lg font-semibold text-gray-800">
+                Device Settings :
+              </h2>
+              <div className="flex gap-2">
+                <div className="flex items-center">
+                  <Dropdown
+                    className=" cursor-pointer "
+                    label={"Camera"}
+                    dismissOnClick
+                    color={"pink"}
+                  >
+                    {videoDevices.map((device) => {
+                      return (
+                        <DropdownItem key={device?.id} className=" ">
+                          {device.label}
+                        </DropdownItem>
+                      );
+                    })}
+                  </Dropdown>
+                </div>
+                <div className="flex items-center">
+                  <Dropdown
+                    className=" cursor-pointer "
+                    label={"Audio"}
+                    dismissOnClick
+                    color={"pink"}
+                  >
+                    {audioDevices.map((device) => {
+                      return (
+                        <DropdownItem key={device?.id} className=" ">
+                          {device.label}
+                        </DropdownItem>
+                      );
+                    })}
+                  </Dropdown>
+                </div>
+              </div>
+            </div>
+          )}
+          {/* Record/Stop */}
+          <div className="flex justify-center">
+            {status === "idle" && (
+              <button
+                className="bg-blue-500 hover:bg-blue-600 rounded-md px-5 py-2 my-2 text-white font-semibold cursor-pointer flex gap-2 w-full text-center items-center justify-center"
+                onClick={handleStartRecording}
+              >
+                <Video /> Record Video
+              </button>
+            )}
+            {status === "recording" && (
+              <button
+                className="bg-red-500 hover:bg-red-600rounded-md px-5 py-2 my-2 ml-2 text-white font-semibold cursor-pointer w-full flex gap-2 justify-center"
+                onClick={stopRecording}
+              >
+                <VideoOff /> Stop Recording
+              </button>
+            )}
+          </div>
+          {/* Questions */}
           {formConfig.collectionType === "Video only" && (
             <>
               <div>
@@ -116,20 +265,22 @@ function VideoReviewModal({ setShowVideoReviewModal, formConfig }) {
             </>
           )}
           {/* Action Buttons */}
-          <div className="flex flex-col sm:flex-row gap-4 mt-4">
-            <button
-              onClick={handleClose}
-              className="w-full sm:w-auto px-6 py-2 rounded-md border cursor-pointer border-gray-300 text-gray-600 bg-white hover:bg-red-500 hover:text-white transition"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={handleSubmit}
-              className="w-full sm:w-auto px-6 py-2 rounded-md cursor-pointer bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
-            >
-              Send
-            </button>
-          </div>
+          {/* {status === "idle" && (
+            <div className="flex flex-col sm:flex-row gap-4 mt-4">
+              <button
+                onClick={handleClose}
+                className="w-full sm:w-auto px-6 py-2 rounded-md border cursor-pointer border-gray-300 text-gray-600 bg-white hover:bg-red-500 hover:text-white transition"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleSubmit}
+                className="w-full sm:w-auto px-6 py-2 rounded-md cursor-pointer bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
+              >
+                Send
+              </button>
+            </div>
+          )} */}
         </div>
       </div>
     </div>
